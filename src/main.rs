45118307feedc237a88d9a09a0a134c679cc075f
micheal-ndoh <michaelndoh9@gmail.com>
2025-06-22 18:56:07 +0100

    feat: refactor AppState structure for improved user management and environment variable loading

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 use axum::{
     routing::{get, post},
     Router,
@@ -5,20 +7,22 @@ use axum::{
 use tower_http::cors::CorsLayer;
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
+
 pub mod middleware;
 pub mod models;
 pub mod routes;
-use crate::middleware::auth::auth_middleware;
-use crate::routes::{auth, protected};
-use dotenv;
-mod utils;
+pub mod utils;
+use dotenvy::dotenv;
 
-use std::sync::{Arc, Mutex};
+use crate::{
+    middleware::auth::auth_middleware,
+    routes::{auth, protected},
+};
 
-// AppState holds all registered users in memory
 #[derive(Debug, Clone)]
 pub struct AppState {
-    pub users: Vec<models::User>,
+    pub config: Arc<utils::Config>,
+    pub users: Arc<Mutex<Vec<models::User>>>,
 }
 
 #[tokio::main]
@@ -26,12 +30,7 @@ async fn main() {
     #[derive(OpenApi)]
     #[openapi(
         info(title = "Auth API", description = "A simple auth API"),
-        paths(
-            auth::login,
-            protected::admin_route,
-            protected::user_route,
-            auth::register
-        ),
+        paths(auth::login, auth::register, protected::admin_route),
         components(schemas(
             models::User,
             models::Role,
@@ -41,22 +40,23 @@ async fn main() {
     )]
     struct ApiDoc;
 
-    dotenv::dotenv().ok(); // Load environment variables from .env file
-                           // Create a thread-safe, shared AppState using Arc<Mutex<...>>
-    let state = Arc::new(Mutex::new(AppState { users: vec![] }));
+    let state = AppState {
+        users: Arc::new(Mutex::new(vec![])),
+        config: Arc::new(utils::load_env()),
+    };
 
-    // Pass the same state to all routes so registration and login share users
     let app = Router::new()
         .route("/admin", get(protected::admin_route))
-        .layer(axum::middleware::from_fn(auth_middleware))
+        .layer(axum::middleware::from_fn_with_state(
+            state.clone(),
+            auth_middleware,
+        ))
         .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi()))
         .route("/login", post(auth::login))
         .route("/register", post(auth::register))
-        .route("/user", get(protected::user_route))
         .layer(CorsLayer::permissive())
         .with_state(state);
 
-    println!("Server running on http://localhost:3000");
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
