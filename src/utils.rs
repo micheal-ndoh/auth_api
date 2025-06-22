1257d6382849362fb28b293d98970ddc20562ef1
micheal-ndoh <michaelndoh9@gmail.com>
2025-06-22 18:59:56 +0100

    feat: refactor Config struct and load_env function for improved JWT handling

--- src/utils.rs
+++ src/utils.rs
@@ -1,26 +1,35 @@
-use core::panic;
+use dotenvy::dotenv;
+
+#[derive(Debug, Clone)]
 pub struct Config {
+    pub jwt_salt: [u8; 16],
     pub jwt_secret: String,
-    pub jwt_salt: String,
-    pub jwt_expiration: String,
+    pub jwt_expiration_secs: u32,
 }
 
 pub fn load_env() -> Config {
-    dotenv::dotenv().ok();
-    let JWT_SECRET = std::env::var("JWT_SECRET").unwrap_or_else(|_| {
-        panic!("JWT_SECRET must be set in the environment");
-    });
+    dotenv().ok();
 
-    let JWT_SALT = std::env::var("JWT_SALT").unwrap_or_else(|_| {
-        panic!("JWT_SALT must be set in the environment");
-    });
-    let JWT_EXPIRATION = std::env::var("JWT_EXPIRATION").unwrap_or_else(|_| {
-        panic!("JWT_EXPIRATION must be set in the environment");
-    });
+    let jwt_salt = std::env::var("JWT_SALT").expect("JWT_SALT environment variable is not set");
+
+    let jwt_salt_bytes = jwt_salt.as_bytes();
+    if jwt_salt.len() != 16 {
+        panic!("JWT_SALT must be 16 bytes long");
+    }
+    let mut jwt_salt = [0u8; 16];
+    jwt_salt.copy_from_slice(&jwt_salt_bytes[0..16]);
+
+    let jwt_secret =
+        std::env::var("JWT_SECRET").expect("JWT_SECRET environment variable is not set");
+
+    let jwt_expiration = std::env::var("JWT_EXPIRATION")
+        .expect("JWT_EXPIRATION environment variable is not set")
+        .parse::<u32>()
+        .expect("JWT_EXPIRATION mustv be a valid unsigned integer");
 
     return Config {
-        jwt_secret: JWT_SECRET,
-        jwt_salt: JWT_SALT,
-        jwt_expiration: JWT_EXPIRATION,
+        jwt_salt,
+        jwt_secret,
+        jwt_expiration_secs: jwt_expiration,
     };
 }
