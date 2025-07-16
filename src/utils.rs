e3de159e33f5d9f4c0d2c76d2ba06fe60e7a1294
micheal-ndoh <michaelndoh9@gmail.com>
2025-07-16 09:07:59 +0100

    feat: add Google client ID and secret to configuration for OAuth integration

--- src/utils.rs
+++ src/utils.rs
@@ -5,6 +5,8 @@ pub struct Config {
     pub jwt_salt: [u8; 16],
     pub jwt_secret: String,
     pub jwt_expiration_secs: u32,
+    pub google_client_id: String,
+    pub google_client_secret: String,
 }
 
 pub fn load_env() -> Config {
@@ -27,9 +29,14 @@ pub fn load_env() -> Config {
         .parse::<u32>()
         .expect("JWT_EXPIRATION mustv be a valid unsigned integer");
 
+    let google_client_id = std::env::var("GOOGLE_CLIENT_ID").expect("GOOGLE_CLIENT_ID environment variable is not set");
+    let google_client_secret = std::env::var("GOOGLE_CLIENT_SECRET").expect("GOOGLE_CLIENT_SECRET environment variable is not set");
+
     return Config {
         jwt_salt,
         jwt_secret,
         jwt_expiration_secs: jwt_expiration,
+        google_client_id,
+        google_client_secret,
     };
 }
